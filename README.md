7e66cf685882eb226778b0d99af70d958e5e9822
castroofelipee <castroofelipee19@gmail.com>
2024-04-26 16:35:05 -0300

    [prod] Building an application with Django and NextJs

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# Django-Rest---NextJs-Web
+# Integração de Django e Next.js 
+- A aplicação é um aplicativo CRUD simples para gerenciar o menu de um restaurante. A partir do frontend, o usuário deve ser capaz de:
+
+    1. listar todos os menus
+    2. recuperar um menu
+    3. criar um menu
+    4. atualizar um menu
+    5. excluir um menu
+
+![alt text](image.png)
+![alt text](image-1.png)
---
+++ next-js-front/src/app/page.js
@@ -0,0 +1,116 @@
+import { useEffect, useState } from "react";
+import { useClient, useNavigation, useSearchParams } from "next/client";
+
+async function deleteMenu(id) {
+  const res = await fetch(`http://127.0.0.1:8000/api/menu/${id}/`, {
+    method: "DELETE",
+  });
+  if (!res.ok) {
+    throw new Error("Failed to retrieve menu");
+  }
+  return Promise.resolve();
+}
+
+async function getData() {
+  const res = await fetch("http://127.0.0.1:8000/api/menu/");
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+  return res.json();
+}
+
+const MenuItem = ({ id, name, price, onEdit, onDelete }) => {
+  return (
+    <div className="menu-item" data-id={id}>
+      <div className="menu-item-info">
+        <div className="menu-item-name">{name}</div>
+        <div className="menu-item-price">${price.toFixed(2)}</div>
+      </div>
+      <div className="menu-item-actions">
+        <button className="edit-button" onClick={onEdit}>
+          Edit
+        </button>
+        <button
+          className="delete-button"
+          onClick={() => {
+            deleteMenu(id).then(() => onDelete(id));
+          }}
+        >
+          Delete
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default function Page() {
+  useClient(); // Marca o componente pai com "useClient"
+
+  const [menuItems, setMenuItems] = useState(null);
+  const navigation = useNavigation();
+  const params = useSearchParams();
+
+  const [displaySuccessMessage, setDisplaySuccessMessage] = useState({
+    show: false,
+    type: "",
+  });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await getData();
+      setMenuItems(data);
+    };
+    fetchData().catch(console.error);
+  }, []);
+
+  useEffect(() => {
+    if (!!params.get("action")) {
+      setDisplaySuccessMessage({
+        type: params.get("action"),
+        show: true,
+      });
+      navigation.replace("/");
+    }
+  }, [params, navigation]);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (displaySuccessMessage.show) {
+        setDisplaySuccessMessage({ show: false, type: "" });
+      }
+    }, 3000);
+    return () => clearTimeout(timer);
+  }, [displaySuccessMessage.show]);
+
+  const handleDelete = (id) => {
+    setMenuItems((items) => items.filter((item) => item.id !== id));
+  };
+
+  return (
+    <div>
+      <button className="add-button" onClick={() => navigation.push("/add")}>
+        Add
+      </button>
+      {displaySuccessMessage.show && (
+        <p className="success-message">
+          {displaySuccessMessage.type === "add" ? "Added a" : "Modified a"} menu
+          item.
+        </p>
+      )}
+      {menuItems ? (
+        menuItems.map((item) => (
+          <MenuItem
+            key={item.id}
+            id={item.id}
+            name={item.name}
+            price={item.price}
+            onEdit={() => navigation.push(`/update/${item.id}`)}
+            onDelete={handleDelete}
+          />
+        ))
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
+}
